c16133c4d54fbd976f256817b9681ade310a2a37
Sanjay Rai <sanjay.rai@shivit.in>
2021-01-30 18:53:27 +0530

    added method

--- Apple.java
+++ Apple.java
@@ -2,4 +2,8 @@ public class Main {
 	public static void main(String... args) {
 		System.out.println("Hello World");
 	}
+
+	public void info() {
+		System.out.println("This is code written in bugfix branch");
+	}
 }
